aa2395fb102d0e811ef62f3c7876e9e2208300a6
POTATOX <mustafakr83@gmail.com>
2024-11-08 15:49:39 +0300

    firsttt

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.post('/admin/add-post', authenticateToken, (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'postlarimm.html'));
+    res.sendFile(path.join(__dirname, 'public', 'postlarim.html'));
 });
 
 
